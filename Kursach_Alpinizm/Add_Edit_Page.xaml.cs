f3d56e7813109717f5d46c337c9812b8ab288008
adelgaripov <adelgaripov6@gmail.com>
2024-04-25 00:16:37 +0300

    init

---
+++ Kursach_Alpinizm/Add_Edit_Page.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Collections.Specialized;
+
+
+
+namespace Kursach_Alpinizm
+{
+    /// <summary>
+    /// Логика взаимодействия для Add_Edit_Page.xaml
+    /// </summary>
+    public partial class Add_Edit_Page : Page
+    {
+        private mountain _currentMountain = new mountain();
+        
+        public Add_Edit_Page()
+        {
+            InitializeComponent();
+            DataContext = _currentMountain;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentMountain.Title))
+                errors.AppendLine("Укажите название отеля");
+            if (string.IsNullOrWhiteSpace(_currentMountain.Mountain_range))
+                errors.AppendLine("Укажите название хребта");
+            if (_currentMountain.Height < 372 || _currentMountain.Height > 8848)
+                errors.AppendLine("Высоты с такой горой нет(");
+
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentMountain.ID_mountain == 0)
+                AlpinizmEntities.GetContext().mountain.Add(_currentMountain);
+            try
+            {
+                AlpinizmEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ Kursach_Alpinizm/Page_Mountains.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursach_Alpinizm
+{
+    /// <summary>
+    /// Логика взаимодействия для Page_Mountains.xaml
+    /// </summary>
+    public partial class Page_Mountains : Page
+    {
+        public Page_Mountains()
+        {
+            InitializeComponent();
+            Mountains.ItemsSource = AlpinizmEntities.GetContext().mountain_climbs.ToList();
+        }
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Add_Edit_Page());
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                AlpinizmEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                Mountains.ItemsSource = AlpinizmEntities.GetContext().mountain_climbs.ToList();
+            }
+        }
+    }
+}
